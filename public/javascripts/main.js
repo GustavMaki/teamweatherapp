9be78894d576943f4f2d8c1483e3f7433536e788
GustavMaki <gustav.siljamaki@gmail.com>
2020-10-14 14:06:45 +0200

    js changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,18 @@
+var searched = [];
 document.querySelector("form").addEventListener("submit", e => {
     e.preventDefault();
     let inputCityValue = document.getElementById("txtCity").value;
+
+  var n = searched.includes(inputCityValue);  
   
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCityValue}&appid=d54db95837a99766a279927c50b6fde1&units=metric`)
+  url =`https://api.openweathermap.org/data/2.5/weather?q=${inputCityValue}&appid=d54db95837a99766a279927c50b6fde1&units=metric`;
+  
+  
+
+  if(n === false){
+  searched.push(inputCityValue);
+  document.getElementById("alert").innerHTML= " "
+  fetch(url)
      .then(response => response.json())
      .then(data => {
         const { main, name, sys, weather } = data;
@@ -11,7 +21,7 @@ document.querySelector("form").addEventListener("submit", e => {
         li.classList.add("city");
   
         const markup = `
-          <div>
+          <div id= "result">
               <h2>
                   <span>${name}</span>
                   <sup>${sys.country}</sup>
@@ -19,12 +29,17 @@ document.querySelector("form").addEventListener("submit", e => {
               <div>${main.temp}<sup>°C</sup></div>
           </div>
         `;
-  
+       
         li.innerHTML = markup;
         document.getElementById("cities").appendChild(li);
+        
       })
      .catch(() => {
-      console.log("something went wrong, sorry");
+      document.getElementById("alert").innerHTML= "Doesnt exist!"
     });
-  
+    
+  }
+  else{
+    
+  }
   });
